d17182f6a854493f447b021783d9a552ea072b31
Fabricio Oliveira <13788356+fabriciovo@users.noreply.github.com>
2024-08-30 12:57:25 -0300

    wip animations

---
+++ js/jquery-custom.js
@@ -0,0 +1,16 @@
+jQuery.fn.shake = function (
+  intShakes = 20,
+  intDistance = 2,
+  intDuration = 0.5
+) {
+  this.each(function () {
+    $(this).css("position", "relative");
+    for (var x = 1; x <= intShakes; x++) {
+      $(this)
+        .animate({ left: intDistance * -1 }, intDuration / intShakes / 4)
+        .animate({ left: intDistance }, intDuration / intShakes / 2)
+        .animate({ left: 0 }, intDuration / intShakes / 4);
+    }
+  });
+  return this;
+};
