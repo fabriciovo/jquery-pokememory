a96aae6aa000c6d200ca796edac7fe23e4a5a614
Fabricio Varisco <fabricio.varisco@outlook.com>
2024-08-30 21:47:40 -0300

    shake anim

--- js/jquery-custom.js
+++ js/jquery-custom.js
@@ -1,16 +1,19 @@
 jQuery.fn.shake = function (
   intShakes = 20,
   intDistance = 2,
-  intDuration = 0.5
+  intDuration = 0.5,
+  animFinished = function(){ }
 ) {
-  this.each(function () {
+  this.each(function (_) {
     $(this).css("position", "relative");
     for (var x = 1; x <= intShakes; x++) {
       $(this)
         .animate({ left: intDistance * -1 }, intDuration / intShakes / 4)
         .animate({ left: intDistance }, intDuration / intShakes / 2)
-        .animate({ left: 0 }, intDuration / intShakes / 4);
+        .animate({ left: 0 }, intDuration / intShakes / 4)
     }
+  }).promise().done(function () {
+    animFinished();
   });
   return this;
 };
